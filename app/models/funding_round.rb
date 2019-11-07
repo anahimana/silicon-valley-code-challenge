559a9fa36d41c528ea204cfd67e642267e2b8338
Alex Nahimana <alex.nahimana1@gmail.com>
2019-11-07 11:01:00 -0600

    Fix funding round class

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,16 @@
 class FundingRound
+    attr_accessor :type, :investment
+    attr_reader :startup, :venture_capitalist
+    @@all = []
+    def initialize(type, investment, vc, startup )
+        @type = type
+        @investment = investment
+        @startup = startup
+        @venture_capitalist = vc
+        @@all << self
+    end
 
+    def self.all()
+        @@all
+    end
 end
