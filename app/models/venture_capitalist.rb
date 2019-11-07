b985b32fee9b2a98694c0d5967ba0a979ca67540
Alex Nahimana <alex.nahimana1@gmail.com>
2019-11-07 09:20:07 -0600

    Add venture capitalist class

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,21 @@
 class VentureCapitalist
+    attr_accessor :name, :net_worth
+    @@all = []
+    def initialize(name, net_worth)
+        @name = name
+        @net_worth = net_worth
+        @@all << self
+    end
 
+    def total_worth()
+        @net_worth
+    end
+
+    def self.all()
+        @@all
+    end
+
+    def self.tres_commas_club()
+        self.all.select {|vc| vc.net_worth.to_s.length >= 10}
+    end
 end
