524618ba3e08712419cac6bee97b0fe71a914c5d
Alex Nahimana <alex.nahimana1@gmail.com>
2019-11-06 21:35:12 -0600

    Add startup model

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class Startup
+    attr_accessor :name
+    attr_reader :founder, :domain
+    @@all = []
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
 
+    def pivot(name, domain)
+        @name, @domain = name, domain
+    end
+
+    def self.all()
+        @@all
+    end
+
+    def self.find_by_founder(name)
+        self.all.find {|startup| startup.founder == name}
+    end
+
+    def self.domains()
+        self.all.map {|startup| startup.domain}.uniq
+    end
 end
