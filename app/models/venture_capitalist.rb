a8c8c82f29bd0f1058738ce95c29c16cc9183b32
Alex Nahimana <alex.nahimana1@gmail.com>
2019-11-18 10:45:51 -0600

    Add venture capitalist Associations and Aggregate

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class VentureCapitalist
     attr_accessor :name, :net_worth
     @@all = []
@@ -17,5 +18,32 @@ class VentureCapitalist
 
     def self.tres_commas_club()
         self.all.select {|vc| vc.net_worth.to_s.length >= 10}
+    end
+    
+    def offer_contract(startup, type, amount)
+        FundingRound.new(type, amount, self, startup)
+    end
+
+    def funding_rounds() 
+        rounds = FundingRound.all.select{|round| round.venture_capitalist == self}
+    end
+
+    def portfolio()
+        funding_rounds.map {|round| round.startup}
+    end
+
+    def biggest_investment()
+        max_investment = funding_rounds.map {|round| round.investment}.max
+        funding_rounds.find {|round| round.investment == max_investment}
+    end
+
+    def invested(domain_name)
+        amount = 0
+        funding_rounds.each do |round| 
+            if round.startup.domain == domain_name
+                amount += round.investment
+            end
+        end
+        amount
     end
 end
